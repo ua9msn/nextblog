23aa38aea1b8f26532aaf435fe27fa8ed4a2ede4
Sergey Balykov <ua9msn@gmail.com>
2022-11-27 18:55:49 +0300

    Nextjs Netlify-cms

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  webpack: function (config) {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: 'raw-loader',
-    })
-    return config
-  },
+  swcMinify: true,
 }
+
+module.exports = nextConfig
