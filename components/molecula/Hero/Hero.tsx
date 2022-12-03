f576dc9b8ef28ee90ac78ee1ea3e0bfe74d4c1da
Sergey Balykov <ua9msn@gmail.com>
2022-12-03 16:09:15 +0300

    FIX Hero

--- components/molecula/Hero/Hero.tsx
+++ components/molecula/Hero/Hero.tsx
@@ -11,7 +11,7 @@ interface Button {
 
 interface Props {
 	data: {
-        theme: 'white' | 'brand';
+		theme: 'white' | 'brand';
 		bgImage: boolean;
 		photo: string;
 		textAlign: 'left' | 'right' | 'center';
@@ -41,6 +41,8 @@ export const Hero: React.FC<Props> = (props: Props) => {
 		? `url('https://res.cloudinary.com/littledata/image/upload/f_auto,b_black,o_50/www2/${data.bgImage}')`
 		: 'none';
 
+	const { mainButton, secondaryButton } = data;
+
 	return (
 		<section className={css.hero} style={{ backgroundColor, backgroundImage, color }}>
 			<div className="container mx-auto flex px-4 py-12 sm:py-24 md:flex-row flex-col items-center max-w-screen-xl">
@@ -53,20 +55,22 @@ export const Hero: React.FC<Props> = (props: Props) => {
 					<h1 className="title-font sm:text-5xl text-3xl mb-4 font-medium">{data.title}</h1>
 					<p className="mb-8 leading-relaxed text-xl font-thin">{data.description}</p>
 					<div className={`flex justify-center ${data.textAlign === 'center' && 'mx-auto'}`}>
-						{data.mainButton.text && data.mainButton.link && (
-							<LdLink href={data.mainButton.link} target={data.mainButton.newTab ? '_blank' : ''}>
-								{data.mainButton.text}
-							</LdLink>
-						)}
-						{data.secondaryButton.text && data.secondaryButton.link && (
-							<LdLink
-								type="secondary"
-								href={data.secondaryButton.link}
-								target={data.secondaryButton.newTab ? '_blank' : ''}
-							>
-								{data.secondaryButton.text}
-							</LdLink>
-						)}
+						<>
+							{mainButton && (
+								<LdLink href={mainButton.link} target={mainButton.newTab ? '_blank' : ''}>
+									{mainButton.text}
+								</LdLink>
+							)}
+							{secondaryButton && (
+								<LdLink
+									type="secondary"
+									href={secondaryButton.link}
+									target={secondaryButton.newTab ? '_blank' : ''}
+								>
+									{secondaryButton.text}
+								</LdLink>
+							)}
+						</>
 					</div>
 				</div>
 				<Photo photo={data.photo} />
