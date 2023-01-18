fa94c468753baa50d8e29e27aebe37ecda58628a
SB <ua9msn@gmail.com>
2023-01-18 18:15:15 +0300

    WIP

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,4 +1,5 @@
-import CMS from 'netlify-cms-app'
+import dynamic from "next/dynamic";
+import Head from "next/head";
 
 const MyTemplate = (data: any) => {
     console.log(data);
@@ -9,6 +10,58 @@ const MyTemplate = (data: any) => {
 
 
 // Initialize the CMS object
-CMS.init()
-// Now the registry is available via the CMS object.
-CMS.registerPreviewTemplate('my-template', MyTemplate)
+const config = {
+    config: {
+        backend: {
+            name: 'git-gateway',
+            branch: 'main'
+        },
+        media_folder: 'public/img',
+        public_folder: 'img',
+        collections: [
+            {
+                name: "pages",
+                label: "Pages",
+                files: [{
+                    label: "Home",
+                    name: "home",
+                    file: "content/home.md",
+                    fields: [
+                        {label: "Title", name: "title", widget: "string"},
+                        {label: "Publish Date", name: "date", widget: "datetime"},
+                        {label: "Body", name: "body", widget: "markdown"},
+                        {
+                            label: 'Cats', name: "cats", widget: 'list',
+                            fields: [
+                                {label: "Name", name: "name", widget: "string"},
+                                {label: "Description", name: "description", widget: "text"},
+                            ]
+                        }
+                    ]
+                }]
+            }
+        ]
+    }
+
+}
+
+const CMS = dynamic(
+    () =>
+        import('netlify-cms-app').then((CMS) => {
+            CMS.init(config);
+            CMS.registerPreviewTemplate('caseStudies', MyTemplate);
+        }),
+    { ssr: false }
+);
+
+const Admin = () => (
+    <>
+        <Head>
+            <title>Admin</title>
+            <script type="text/javascript" src="https://identity.netlify.com/v1/netlify-identity-widget.js" defer />
+        </Head>
+        <CMS />
+    </>
+);
+
+export default Admin;
