eb52ea731dc6d28f3e967d971df0fd00e09d016b
SB <ua9msn@gmail.com>
2023-01-19 20:29:08 +0300

    ....

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -1,56 +1,56 @@
-import dynamic from "next/dynamic";
-import Head from "next/head";
-
+import dynamic from 'next/dynamic';
+import Head from 'next/head';
 
 const Loading = () => (
-    <div className="min-h-screen flex items-center justify-center">
-        <p className="text-gray-500 font-semibold text-xl">Loading...</p>
-    </div>
+	<div className="min-h-screen flex items-center justify-center">
+		<p className="text-gray-500 font-semibold text-xl">Loading...</p>
+	</div>
 );
+
+const MyTemplate = (data) => {
+	console.log(data);
+	return <div>AAAA</div>;
+};
 
 // Initialize the CMS object
 const config = {
-        backend: {
-            name: 'git-gateway',
-            branch: 'main'
-        },
-        media_folder: 'public/img',
-        public_folder: 'img',
-        collections: [
-            {
-                name: "pages",
-                label: "Pages",
-                files: [{
-                    label: "Home",
-                    name: "home",
-                    file: "content/home.md",
-                    fields: [
-                        {label: "Title", name: "title", widget: "string"},
-                        {label: "Publish Date", name: "date", widget: "datetime"},
-                        {label: "Body", name: "body", widget: "markdown"},
-                        {
-                            label: 'Cats', name: "cats", widget: 'list',
-                            fields: [
-                                {label: "Name", name: "name", widget: "string"},
-                                {label: "Description", name: "description", widget: "text"},
-                            ]
-                        }
-                    ]
-                }]
-            }
-        ]
-}
+	backend: {
+		name: 'git-gateway',
+		branch: 'main',
+	},
+	media_folder: 'public/img',
+	public_folder: 'img',
+	collections: [
+		{
+			name: 'pages',
+			label: 'Pages',
+			folder: 'content/blog',
+			create: true,
+			files: [
+				{
+					label: 'Home',
+					name: 'home',
+					file: 'content/home.md',
+					fields: [
+						{ label: 'Title', name: 'title', widget: 'string', default: 'asdf' }
+					],
+				},
+			],
+		},
+	],
+};
 
 const CMS = dynamic(
-    () =>
-        import('netlify-cms-app').then((cms) => {
-            cms.init({config});
-        }),
-    { ssr: false, loading: () => <p>Loading...</p> }
+	() =>
+		import('netlify-cms-app').then((cms) => {
+			cms.init({ config });
+			cms.registerPreviewTemplate('my-template', MyTemplate)
+		}),
+	{ ssr: false, loading: Loading }
 );
 
 const AdminPage = () => {
-    return <CMS />;
+	return <CMS />;
 };
 
 export default AdminPage;
